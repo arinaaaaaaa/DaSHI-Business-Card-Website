71370641522360b111a248bc9699ff75c1446d38
Obisessionprime <obisessionprime@gmail.com>
2022-03-26 09:39:01 +0300

    Removed unused files and dirs

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,7 +6,7 @@ from news import urls as newsURL
 from partners import urls as partnersURL
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('news/', include(newsURL)),
-    path('partners/', include(partnersURL))
+    path('ds-api/admin/', admin.site.urls),
+    path('ds-api/news/', include(newsURL)),
+    path('ds-api/partners/', include(partnersURL))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
