5bcae730e2989f540a08b36abfdb1340c7b1a576
Obisessionprime <obisessionprime@gmail.com>
2022-03-29 21:29:14 +0300

    Поправил ошибку с utl

--- frontend/components/universal/LeftMenu.js
+++ frontend/components/universal/LeftMenu.js
@@ -12,36 +12,34 @@ function Menu(props) {
             <div className = {styles.linksSection}>
                 {menuType == 0 ?
                     <>
-                        <Link href="#"><a>Стать партнером DS</a></Link>
-                        <Link href="#">
-                            <a>Запчасти DS в наличии
-                                <span  className="tooltip">
-                                    <span className="tooltipIcon dark" style={{marginLeft: 11 + "px"}}>?</span>
-                                    <span className="promt">
-                                        На сайтах наших партнеров и представителей DS в разделе «Продукция DaSHI» вы сможете
-                                        проследить путь каждой детали – запуск в производство («На заводе»), срок поставки и
-                                        местонахождение («В пути»), размещение на складе в России («В наличии»).
-                                    </span>
+                        <a href="#">Стать партнером DS</a>
+                        <a href="#">Запчасти DS в наличии
+                            <span  className="tooltip">
+                                <span className="tooltipIcon dark" style={{marginLeft: 11 + "px"}}>?</span>
+                                <span className="promt">
+                                    На сайтах наших партнеров и представителей DS в разделе «Продукция DaSHI» вы сможете
+                                    проследить путь каждой детали – запуск в производство («На заводе»), срок поставки и
+                                    местонахождение («В пути»), размещение на складе в России («В наличии»).
                                 </span>
-                            </a>
-                        </Link>
+                            </span>
+                        </a>
                     </>
                 : menuType == 1 ?
-                    <Link href={`${process.env.api_hostname}/buyers/`}><a>Склады DS в России и других странах</a></Link>
+                    <Link href="/buyers"><a>Склады DS в России и других странах</a></Link>
                 : menuType == 2 ?
                     <>
-                        <Link href={`${process.env.api_hostname}/partners/`}><a>Стать партнером DS</a></Link>
-                        <Link href={`${process.env.api_hostname}/partners/`}><a>Пакет &quot;DS+&quot;</a></Link>
-                        <Link href={`${process.env.api_hostname}/guarantee/`}><a style={{marginTop: 20 + 'px'}}>Упаковка DS</a></Link>
+                        <Link href="/partners"><a>Стать партнером DS</a></Link>
+                        <Link href="/partners"><a>Пакет &quot;DS+&quot;</a></Link>
+                        <Link href="/guarantee"><a style={{marginTop: 20 + 'px'}}>Упаковка DS</a></Link>
                     </>
                 : "" }
             </div>
             <div className = {styles.feedbackSection}>
                 <div className={styles.imgLinks}>
-                    <Link href="#"><a><img src="icons/world.svg" alt=""/></a></Link>
-                    <Link href="#"><a><img src="icons/headset.svg" alt=""/></a></Link>
-                    <Link href="#"><a><img src="icons/phone.svg" alt=""/></a></Link>
-                    <Link href="#"><a><img src="icons/mail.svg" alt=""/></a></Link>
+                    <Link href="#"><a><img src="icons/world.svg" alt="" /></a></Link>
+                    <Link href="#"><a><img src="icons/headset.svg" alt="" /></a></Link>
+                    <Link href="#"><a><img src="icons/phone.svg" alt="" /></a></Link>
+                    <Link href="#"><a><img src="icons/mail.svg" alt="" /></a></Link>
                 </div>
                 <Link href="#"><a className={styles.feedbackLink}>Форма обратной связи</a></Link>
             </div>
